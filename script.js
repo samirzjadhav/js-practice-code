23b6048effaf847fa28ce5c08ab96bb8097845ad
samirzjadhav <samirzjadhav@gmail.com>
2025-10-01 11:48:00 +0530

    feat: create simplte todo

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = document.getElementById("input");
+const add = document.getElementById("add");
+const tasklist = document.querySelector(".tasklist");
+
+add.addEventListener("click", () => {
+  const inputValue = input.value.trim();
+  console.log(inputValue);
+
+  if (inputValue) {
+    const li = document.createElement("li");
+    li.textContent = inputValue;
+
+    tasklist.appendChild(li);
+    input.value = "";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.textContent = "x";
+
+    li.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", () => {
+      li.remove();
+    });
+  }
+});
